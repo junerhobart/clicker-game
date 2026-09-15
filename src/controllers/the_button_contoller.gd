470223e40e64a8c399b0d033e21b7fdbb333ab61
June Hobart <me@junehobart.com>
2026-09-15 22:35:50 +0100

    feat: added rebirths

--- src/controllers/the_button_contoller.gd
+++ src/controllers/the_button_contoller.gd
@@ -6,6 +6,8 @@ const ButtonEffectsModule = preload("res://src/modules/button_effects.gd")
 @onready var money_label = $"../CurrencyContainer"
 @onready var click_upgrade = $"../Upgrades/MoneyPerClick"
 @onready var clicksfx: AudioStreamPlayer2D = $"../SFX/Click"
+@onready var rebirth = $"../Upgrades/Rebirth"
+
 var effects: ButtonEffectsModule
 
 func _input(event: InputEvent) -> void:
@@ -29,7 +31,7 @@ func _ready() -> void:
 	pressed.connect(effects.play_pressed_effect.bind(self))
 	button_down.connect(clicksfx.play)
 	pressed.connect(func() -> void:
-		money_label.money += click_upgrade.money_per_click
+		money_label.money += int(click_upgrade.money_per_click * rebirth.money_multiplier)
 	)
 
 	mouse_exited.connect(effects.play_released_effect.bind(self))
---
+++ src/controllers/upgrades/rebirth.gd
@@ -0,0 +1,45 @@
+extends Button
+
+const ButtonEffectsModule = preload("res://src/modules/button_effects.gd")
+var effects = ButtonEffectsModule.new()
+
+@onready var money = $"../../CurrencyContainer"
+@onready var sfx = $"../../SFX"
+@onready var money_per_click = $"../MoneyPerClick"
+@onready var planet_button = $"../../The Button"
+
+var planet_assetsdict = "res://assets/planets/"
+
+var money_multiplier: float = 1.0
+
+var cost: int = 0
+
+var rebirth_count: int = 1
+
+func wipe() -> void:
+	money.money = 0
+	money_per_click.money_per_click = 1
+	money_per_click.cost = 1
+	money_per_click.updateText()
+
+func _ready() -> void:
+	pivot_offset_ratio = Vector2(0.5, 0.5)
+
+	pressed.connect(func() -> void:
+		if cost <= money.money and rebirth_count < 9:
+			sfx.confirmation()
+			wipe()
+			money_multiplier *= 2
+			cost *= 2
+			rebirth_count += 1
+			planet_button.icon = load("res://assets/planets/planet%02d.png" % rebirth_count)
+		else:
+			sfx.error()
+	)
+	button_down.connect(effects.play_pressing_effect.bind(self))
+	pressed.connect(effects.play_pressed_effect.bind(self))
+	mouse_exited.connect(effects.play_released_effect.bind(self))
+	mouse_entered.connect(effects.play_hover_effect.bind(self))
+
+func _process(_delta) -> void:
+	text = "$" + str(cost) + " Rebirth"
